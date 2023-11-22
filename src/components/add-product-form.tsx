f9c3b65a2b7247d47a3ec9b18cf95786971510fd
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-22 17:17:25 -0300

    feat: add combobox for categories

--- src/components/add-product-form.tsx
+++ src/components/add-product-form.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -21,9 +22,28 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import { Input } from "@/components/ui/input";
 import { handleCreateProduct } from "@/services/create";
 import { useState } from "react";
+
+import { categories } from "@/data";
+import { Label } from "./ui/label";
+import { ScrollArea } from "./ui/scroll-area";
 
 const formSchema = z.object({
   name: z.string().min(2),
@@ -37,6 +57,9 @@ export type FormSchema = z.infer<typeof formSchema>;
 
 export function AddProductForm() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("2");
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -52,10 +75,19 @@ export function AddProductForm() {
   async function onSubmit(values: FormSchema) {
     setIsLoading(true);
 
+    const product = {
+      name: values.name,
+      category: Number(value),
+      base_price: values.base_price,
+      discount_percentage: values.discount_percentage,
+      image_url: values.image_url,
+    };
+
     try {
-      await handleCreateProduct(values);
+      await handleCreateProduct(product);
 
       form.reset();
+      setValue("2");
     } catch (error) {
       window.alert("Erro ao criar produto!");
     } finally {
@@ -91,18 +123,59 @@ export function AddProductForm() {
               )}
             />
 
-            <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Categoria:</FormLabel>
-                  <FormControl>
-                    <Input autoComplete="off" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <Label className="-mb-2">Categoria:</Label>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-full justify-between"
+                >
+                  {value
+                    ? categories.find(
+                        (category) => category.value.toString() === value
+                      )?.label
+                    : "Selecione categoria..."}
+                  <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[334px] p-0">
+                <Command className="w-full">
+                  <CommandInput
+                    placeholder="Procurar categoria..."
+                    className="h-9"
+                  />
+                  <CommandEmpty>Nenhuma categoria encontrada.</CommandEmpty>
+                  <CommandGroup>
+                    <ScrollArea className="h-40">
+                      {categories.map((category) => (
+                        <CommandItem
+                          key={category.value}
+                          value={category.value.toString()}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                          {category.label}
+                          <CheckIcon
+                            className={cn(
+                              "ml-auto h-4 w-4",
+                              value === category.value.toString()
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
+                    </ScrollArea>
+                  </CommandGroup>
+                </Command>
+              </PopoverContent>
+            </Popover>
 
             <FormField
               control={form.control}
---
+++ src/data/index.ts
@@ -0,0 +1,110 @@
+export const categories = [
+  {
+    value: 1,
+    label: "Petshop",
+  },
+  {
+    value: 2,
+    label: "Alimentos Básicos",
+  },
+  {
+    value: 3,
+    label: "Bebidas",
+  },
+  {
+    value: 4,
+    label: "Cabelo",
+  },
+  {
+    value: 5,
+    label: "Guloseimas",
+  },
+  {
+    value: 6,
+    label: "Enlatados",
+  },
+  {
+    value: 7,
+    label: "Farinhas",
+  },
+  {
+    value: 8,
+    label: "Frios e Congelados",
+  },
+  {
+    value: 9,
+    label: "Hort Fruit",
+  },
+  {
+    value: 10,
+    label: "Limpeza Bucal",
+  },
+  {
+    value: 11,
+    label: "Limpeza Casa",
+  },
+  {
+    value: 12,
+    label: "Limpeza Corporal",
+  },
+  {
+    value: 13,
+    label: "Molhos",
+  },
+  {
+    value: 14,
+    label: "Padaria",
+  },
+  {
+    value: 15,
+    label: "Pós",
+  },
+  {
+    value: 16,
+    label: "Salgadinhos",
+  },
+  {
+    value: 17,
+    label: "Temperos",
+  },
+  {
+    value: 18,
+    label: "Itens Domésticos",
+  },
+  {
+    value: 19,
+    label: "Veículos",
+  },
+  {
+    value: 20,
+    label: "Material Escolar",
+  },
+  {
+    value: 21,
+    label: "Massas",
+  },
+  {
+    value: 22,
+    label: "Óleos",
+  },
+  {
+    value: 23,
+    label: "Churrasco",
+  },
+  {
+    value: 24,
+    label: "Açougue",
+  },
+  {
+    value: 25,
+    label: "Leites",
+  },
+  {
+    value: 26,
+    label: "Biscoitos",
+  },
+  {
+    value: 27,
+    label: "Limpeza Roupas",
+  },
+];
